f5c399e2e81cfc0fe796d54cc66c8339d45ddf24
bibinraj <bibin301@gmail.com>
2018-09-18 21:19:14 +0530

    new changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require ('express');
+const passport = require ('passport');
+const GoogleStrategy = require ('passport-google-oauth20').Strategy;    
+
 const app = express();
-app.get('/', (req,res)=>{
-    res.send({hi:'bibin'});
-});
+passport.use(new GoogleStrategy());
+// app.get('/', (req,res)=>{
+//     res.send({hi:'bibin'});
+// });
 
 const PORT = process.env.PORT || 5000
 
