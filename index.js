f49051ca1131cf1335135e67d81ae851c9279d43
bibinraj <bibin301@gmail.com>
2018-10-03 18:48:04 +0530

    customizing the build process

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app);    
 require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production'){
+
+    // Express will serve up production assets
+    //like our main.js  file , or main.css file !
+app.use(express.static('client/build'))
+    //Express will serve up the index.html file
+    //if it doesn't recognize the route
+const path = require('path');
+
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
