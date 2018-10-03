9e6237ae55c174c1424ca4f32312b0825b201657
bibinraj <bibin301@gmail.com>
2018-10-03 18:48:49 +0530

    customizing

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.use(express.static('client/build'))
     //if it doesn't recognize the route
 const path = require('path');
 
+app.get('*' ,(req ,res)=>{
+    res.sendFile(path.resolve(__dirname , 'client' , 'build' ,'index.html'))
+})
+
 }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
