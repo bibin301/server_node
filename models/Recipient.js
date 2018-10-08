b6b3f9302e7e14377e856e92d59ba24ba538c3cd
bibinraj <bibin301@gmail.com>
2018-10-08 20:19:56 +0530

    mongose

---
+++ models/Recipient.js
@@ -0,0 +1,11 @@
+const mangoose = require('mongoose');
+
+const recipientSchema = new Schema({
+    email : String,
+    responded : {
+        type: Boolean , default: false
+    }
+});
+
+
+module.exports = recipientSchema;
